f0b088842e2631b313b3e697cf0a440b138c8d62
Phan Dinh Phu <phuphandinh2004@gmail.com>
2025-03-15 23:15:49 -0400

    Add Products/Users model

--- Data/ApplicationDbContext.cs
+++ Data/ApplicationDbContext.cs
@@ -1,9 +1,13 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using WebAppTest.Models;
 
 namespace WebAppTest.Data
 {
     public class ApplicationDbContext : DbContext
     {
+        public DbSet<User> Users { get; set; }
+        public DbSet<Product> Products { get; set; }
+
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
             : base(options)
         {
